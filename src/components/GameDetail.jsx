93f704325056874d38814bd5f1dfc7cd3c995d77
jwm0107 <feller0107@gmail.com>
2022-05-12 22:07:32 +0900

    333

--- src/components/GameDetail.jsx
+++ src/components/GameDetail.jsx
@@ -46,7 +46,7 @@ export default function MainFlex() {
   }, []);
   /* ------------ connect wallet --------------*/
   useEffect(() => {
-    const address = `https://bnbextra.xyz?ref=${account}`;
+    const address = `https://fanta.feller?ref=${account}`;
     setReferalAddress(address);
     console.log("account changed", address);
   }, [account]);
